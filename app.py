b8f3f972cfeaaf686beed1f0a4f96c022c9fcbc2
apple-piewhynot126 <apparentlypieces789@gmail.com>
2026-08-19 18:17:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ def sos():
     return "SOS received!", 200
 
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "Miaesha Addun from 9-DARWIN, SJCSHS. Hello, world!"
-    return "SOS backend is running! May God bless you."
+    return render_template("index.html") 
 
 
 if __name__ == "__main__":
