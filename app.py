b1cbf028df96169ae9ad955b27227eb6d1528dc5
apple-piewhynot126 <apparentlypieces789@gmail.com>
2026-08-19 19:43:55 +0800

    Update app.py
    fixing
    -miaesha a.

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
 @app.route("/")
 def home():
     return render_template("webSOS.html")
-
 
 @app.route("/sos", methods=["POST"])
 def sos():
     print("🚨 SOS RECEIVED!")
     return "SOS received!", 200
-
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("webSOS.html") 
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
